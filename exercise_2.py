9490d40d614a160b0f1593d34d913213e9465c14
Plidher <65611683+PLNSTD@users.noreply.github.com>
2024-10-08 17:19:26 -0400

    tried to fix warning on numpy

--- exercise_2.py
+++ exercise_2.py
@@ -1,6 +1,6 @@
 # Exercise 2
 # Install numpy and create a 1D array of numbers from 1 to 5
-import numpy as np
+import numpy as np # type: ignore
 
 myNumpyArray = np.array(range(1,6))
 print(myNumpyArray)
